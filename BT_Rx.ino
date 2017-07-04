7624295fd9ef84c40bdecdb678239e74c28599c7
LEE Dong Gyu <chandong83@naver.com>
2017-07-04 14:02:17 +0900

    Add files via upload

---
+++ BT_Rx.ino
@@ -0,0 +1,27 @@
+#include "AsulBT.h"
+//for joystick
+#define TOLERANCE   10
+#define CENTER_POS  510
+#define IS_CENTER(x) ((x >= (CENTER_POS-TOLERANCE))&&(x <= (CENTER_POS+TOLERANCE)))
+
+AsulBT bt;
+JOYSTICK_DATA joyData;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  bt.begin(9600);
+}
+
+void loop() {
+    if(bt.update() == CMD_JOYSTICK)
+    {
+      if(bt.readJoystick(&joyData))
+      {        
+        Serial.print(joyData.xAxis);        
+        Serial.print(",");        
+        Serial.print(joyData.yAxis);      
+        Serial.print(",");         
+        Serial.println(joyData.btn);
+      }
+    }    
+}
